d0f1b7eb947a0b404b588558a757f9017043b709
Victor Hugo Dorta <vhdorta@hotmail.com>
2023-11-11 14:46:25 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+function validar(){
+    var elemento1 = document.getElementById('valor1').value;
+   var elemento2 = document.getElementById('valor2').value;
+   elemento1 = parseFloat(elemento1);
+   elemento2 = parseFloat(elemento2);
+
+   const mensagemsucesso = ("Validação concluída, pois o campo B está maior que o campo A");
+    const mensagemfalha = ("Campo B menor que Campo A, tente novamente");
+
+if (elemento1 >= elemento2) {
+    const stylefalha = document.querySelector('.mensagemdefalha');
+        stylefalha.innerHTML = mensagemfalha;
+        stylefalha.style.display = 'block';
+        setTimeout(function(){
+            location.reload();
+        }, 2000);
+    }
+    else{
+        const stylesucesso = document.querySelector('.mensagemdesucesso');
+    stylesucesso.innerHTML = mensagemsucesso;
+    stylesucesso.style.display = 'block';
+    }
+    setTimeout(function(){
+        location.reload();
+    }, 2000);
+    return false;
+}
+
+
+
+
+
